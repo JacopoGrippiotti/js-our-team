6297788789334ea83559b7c01fcf2f5e1ee01734
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-29 17:03:22 +0200

    fixed script.js file to complete MILESTONE 0

---
+++ script.js
@@ -0,0 +1,43 @@
+const listOfTeamMembers = [
+    {
+        name: 'Wayne Barnett',
+        role: 'founder & CEO',
+        photo: 'img/wayne-barnett-founder-ceo.jpg'
+    },
+    
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg'
+    },
+    
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'img/walter-gordon-office-manager.jpg'
+    },
+    
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media',
+        photo: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'img/scott-estrada-developer.jpg'
+    },
+    
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'img/barbara-ramos-graphic-designer.jpg'
+    }
+    
+    
+]
+
+
+
+
